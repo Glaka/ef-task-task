ebbf4d5ab4e88d477eef3f0a6555910bdb07539e
gladchenko <a.gladchenko@futuro.team>
2023-03-26 21:05:25 +0300

    adjust text lines to design

--- src/shared/components/Menu/MenuItem.js
+++ src/shared/components/Menu/MenuItem.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const MenuItem = ({ title, url, pathname }) => (
+const MenuItem = ({ isActive, children }) => (
     <li
         className={`menu__list-item ${
-            pathname === url ? 'menu__list-item--active' : ''
+            isActive ? 'menu__list-item--active' : ''
         }`}
     >
-        <Link to={url} className="menu__list-item-link">
-            {title}
-        </Link>
+        {children}
     </li>
 );
 
--- src/shared/components/Menu/helpers.js
+++ src/shared/components/Menu/helpers.js
@@ -1,5 +1,7 @@
 import { useMemo } from 'react';
+import { Link } from 'react-router-dom';
 import MenuItem from './MenuItem';
+const MAX_LINE_LENGTH = 12;
 
 export const initialGetMenuItemsValues = {
     filters: {},
@@ -8,6 +10,10 @@ export const initialGetMenuItemsValues = {
 };
 
 const normalizeUrl = (url) => (url.includes('/') ? url : `/${url}`);
+const formatText = (text) => {
+    if (text.length > MAX_LINE_LENGTH) return text.split(' ');
+    return [text];
+};
 
 export const useNormalizeData = ({ dataReady, data }) =>
     useMemo(() => {
@@ -18,6 +24,7 @@ export const useNormalizeData = ({ dataReady, data }) =>
                       attributes: {
                           ...i.attributes,
                           url: normalizeUrl(i.attributes.url),
+                          title: formatText(i.attributes.title),
                       },
                   };
               })
@@ -28,13 +35,35 @@ export const useMapMenuItems = ({ normalizedData, pathname }) =>
     useMemo(() => {
         return normalizedData.map((i) => {
             const { title, url } = i.attributes;
+            const isActive = pathname === url;
+
+            const renderLinkLines = (() => {
+                return title?.length
+                    ? title.map((line) => {
+                          return (
+                              <Link
+                                  key={`${i.id}${line}`}
+                                  to={url}
+                                  className="menu__list-item-link"
+                              >
+                                  <p className="menu__list-item-link-text">
+                                      {line}
+                                  </p>
+                              </Link>
+                          );
+                      })
+                    : null;
+            })();
+
             return (
                 <MenuItem
                     key={i.id}
                     title={title}
                     url={url}
-                    pathname={pathname}
-                />
+                    isActive={isActive}
+                >
+                    {renderLinkLines}
+                </MenuItem>
             );
         });
     }, [normalizedData, pathname]);
