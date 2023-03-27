9e547045fd114658181cb04f487f95458f342d06
gladchenko <a.gladchenko@futuro.team>
2023-03-27 13:02:58 +0300

    polish 🧹

--- src/shared/components/Menu/helpers.js
+++ src/shared/components/Menu/helpers.js
@@ -31,29 +31,28 @@ export const useNormalizeData = ({ dataReady, data }) =>
             : [];
     }, [data, dataReady]);
 
+const renderLinkLines = ({ title, id, url }) => {
+    return title?.length
+        ? title.map((line) => {
+              return (
+                  <Link
+                      key={`${id}${line}`}
+                      to={url}
+                      className="menu__list-item-link"
+                  >
+                      <p className="menu__list-item-link-text">{line}</p>
+                  </Link>
+              );
+          })
+        : null;
+};
+
 export const useMapMenuItems = ({ normalizedData, pathname }) =>
     useMemo(() => {
         return normalizedData.map((i) => {
             const { title, url } = i.attributes;
             const isActive = pathname === url;
-
-            const renderLinkLines = (() => {
-                return title?.length
-                    ? title.map((line) => {
-                          return (
-                              <Link
-                                  key={`${i.id}${line}`}
-                                  to={url}
-                                  className="menu__list-item-link"
-                              >
-                                  <p className="menu__list-item-link-text">
-                                      {line}
-                                  </p>
-                              </Link>
-                          );
-                      })
-                    : null;
-            })();
+            const linkLines = renderLinkLines({ title, url, id: i.id });
 
             return (
                 <MenuItem
@@ -62,7 +61,7 @@ export const useMapMenuItems = ({ normalizedData, pathname }) =>
                     url={url}
                     isActive={isActive}
                 >
-                    {renderLinkLines}
+                    {linkLines}
                 </MenuItem>
             );
         });
