75b9f575aa72036d4e3083e659e601002bd2bf57
gladchenko <a.gladchenko@futuro.team>
2023-03-29 13:22:36 +0300

    add callback

--- src/shared/components/ErrorMessage/ErrorMessageContainer.tsx
+++ src/shared/components/ErrorMessage/ErrorMessageContainer.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import ErrorMessageComponent from './ErrorMessageComponent';
 import { ErrorMessageContainerProps } from './types';
 
 const ErrorMessageContainer = ({ error }: ErrorMessageContainerProps) => {
-    const refreshPage = () => window.location.reload();
+    const refreshPage = useCallback(() => {
+        window.location.reload();
+    }, []);
 
     return <ErrorMessageComponent error={error} refreshPage={refreshPage} />;
 };
